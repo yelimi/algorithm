8c7a7d45e4c480e455c814c6ab612791964d3a70
yerimi <laura0319@hanmail.net>
2020-01-07 23:51:11 +0900

    2020-01-07

---
+++ PriorityQueue/baekjoon_11279.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine()); // 연산의 개수.
+		PriorityQueue pq = new PriorityQueue(Collections.reverseOrder());
+		
+		for(int i=0;i<n;i++) { 
+			int x = Integer.parseInt(br.readLine());
+			
+			if(pq.isEmpty() && x == 0)
+				System.out.println("0");
+			else if(!pq.isEmpty() && x == 0)
+				System.out.println((int)pq.poll());
+			else
+				pq.offer(x);
+		}
+		
+	}
+}
+
+
