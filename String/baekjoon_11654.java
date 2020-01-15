4d000b90e1ec0e43d7cd0b4c5e0613c6a01afbb2
yerimi <laura0319@hanmail.net>
2020-01-16 00:39:49 +0900

    2020-01-16

---
+++ String/baekjoon_11654.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String n = sc.next();
+		
+		int result = n.charAt(0);
+		System.out.println(result);
+	}
+}
