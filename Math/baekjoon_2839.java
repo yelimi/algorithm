012eec12f3e25a50f89624198dee34199afb9d9b
yerimi <laura0319@hanmail.net>
2020-01-27 15:23:02 +0900

    2020-01-27

---
+++ Math/baekjoon_2839.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		if(n%5==0) {
+			System.out.println(n/5);
+			return;
+		}else{
+			int x = n/5;
+			for(int i=x;i>0;i--) {
+				int temp = n-(i*5);
+				if(temp%3==0) {
+					System.out.println(i+(temp/3));
+					return;
+				}
+			}
+		}
+		
+		if(n%3==0) {
+			System.out.println(n/3);
+            return;
+		}
+		else {
+			System.out.println(-1);
+            return;
+		}
+		
+	}
+}
