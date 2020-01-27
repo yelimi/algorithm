cc8830502edfa49b2d2d098d6fb170e6a00f3fd8
yerimi <laura0319@hanmail.net>
2020-01-27 14:52:00 +0900

    2020-01-27

---
+++ Math/baekjoon_1712.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int i = 0;
+        
+		if(b>=c)
+			System.out.println("-1");
+		else {
+			i = a / (c - b) + 1;
+			System.out.println(i);
+		}
+        
+	}
+}
