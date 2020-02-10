9420e51d8f9af46bbc01e76948240d83dde1af33
yerimi <laura0319@hanmail.net>
2020-02-10 14:41:57 +0900

    2020-02-10

---
+++ Math/Programmers_최대공약수와최소공배수.java
@@ -0,0 +1,26 @@
+class Solution {
+  public int[] solution(int n, int m) {
+      int[] answer = new int[2];
+      
+      if(n > m){
+          int temp = n;
+          n = m;
+          m = temp;
+      }
+      
+      //최대공약수 
+      for(int i=1;i<=n;i++){
+          if((n % i == 0) && (m % i == 0))
+              answer[0] = i;
+      }
+      
+      //최소공배수 
+      if(m % n == 0)
+          answer[1] = m;
+      else
+          answer[1] = (m * n) / answer[0];
+      
+      return answer;
+      
+  }
+}
