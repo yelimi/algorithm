f783221ba5e76dd8075de55e1d3b61ed66f2ca8b
yerimi <laura0319@hanmail.net>
2020-05-07 20:25:48 +0900

    2020-05-07

---
+++ Math/baekjoon_3046.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int r1 = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+		
+		int r2 = (s * 2) - r1;
+		System.out.print(r2);
+		
+	}
+}
+
+
